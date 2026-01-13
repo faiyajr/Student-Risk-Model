5dcc7b260a7c53b6444f3489c572cc4d864d3986
Faiyaj Rahman <faiyajr@umich.edu>
2026-01-12 20:07:04 -0500

    Initial commit

---
+++ src/features.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def prepare_features(df: pd.DataFrame):
+    """
+    Converts raw student data into model-ready features.
+    """
+
+
+    features = df[[
+        "attendance_rate",
+        "avg_assignment_score",
+        "avg_exam_score",
+        "late_submissions"
+    ]]
+
+    labels = df["risk_status"]
+
+    return features, labels
